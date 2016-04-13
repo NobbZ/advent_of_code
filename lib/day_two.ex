94f00b63a7b04be87ac87581c2c94eca81b9399e
Norbert Melzer <timmelzer@gmail.com>
2016-04-13 13:25:57 +0200

    Day 2b

--- lib/day_two.ex
+++ lib/day_two.ex
@@ -7,6 +7,14 @@ defmodule AdventOfCode.DayTwo do
     |> Enum.sum
   end
 
+  def b(input) do
+    input
+    |> String.split
+    |> Enum.map(&parse_to_triple/1)
+    |> Enum.map(&calc_ribbon_length/1)
+    |> Enum.sum
+  end
+
   defp parse_to_triple(input) do
     [l, w, h] = input |> String.split("x")
     {String.to_integer(l), String.to_integer(w), String.to_integer(h)}
@@ -22,5 +30,13 @@ defmodule AdventOfCode.DayTwo do
 
   defp get_areas({l, w, h}) do
     [l * w, l * h, w * h]
+  end
+
+  defp calc_ribbon_length({l, w, h}) do
+    [l, w, h]
+    |> Enum.sort
+    |> Enum.take(2)
+    |> (fn [a, b] -> 2 * (a + b) end).()
+    |> (fn x -> x + l * w * h end).()
   end
 end
