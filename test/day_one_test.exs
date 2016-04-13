5ff521dd85e18d95d8a3e55fced1a281c44d1d4d
Norbert Melzer <timmelzer@gmail.com>
2016-04-13 11:26:44 +0200

    Even more metaprogramming tests

--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -1,34 +1,31 @@
-defmodule DayOneATests do
-  use PowerAssert, async: true
+defmodule AdventOfCodeTests do
 
   @testdata [
-    {"(())", 0},
-    {"()()", 0},
-    {"(((", 3},
-    {"(()(()(", 3},
-    {"))(((((", 3},
-    {"())", -1},
-    {"))(", -1},
-    {")))", -3},
-    {")())())", -3}
+    {DayOne, [
+        a: [
+          {"(())", 0}, {"()()", 0}, {"(((", 3}, {"(()(()(", 3}, {"))(((((", 3},
+          {"())", -1}, {"))(", -1}, {")))", -3}, {")())())", -3}
+        ],
+        b: [
+          {")", 1}, {"()())", 5},
+        ]
+      ]
+    },
   ]
 
-  for {input, exp} <- @testdata do
-    name = "example #{input} is #{exp}"
-    test name, do: assert unquote(exp) = AdventOfCode.DayOne.a unquote(input)
-  end
-end
-
-defmodule DayOneBTests do
-  use PowerAssert, async: true
-
-  @testdata [
-    {")", 1},
-    {"()())", 5},
-  ]
-
-  for {input, exp} <- @testdata do
-    name = "example #{input} is #{exp}"
-    test name, do: assert unquote(exp) = AdventOfCode.DayOne.b unquote(input)
+  for {module, funs} <- @testdata do
+    defmodule module do
+      use PowerAssert, async: true
+      for {fun, tests} <- funs do
+        for {input, exp} <- tests do
+          name = "#{fun}: example #{input} is #{exp}"
+          test name do
+            assert unquote(exp) = apply(AdventOfCode.unquote(module),
+                                        unquote(fun),
+                                        [unquote(input)])
+          end
+        end
+      end
+    end
   end
 end
