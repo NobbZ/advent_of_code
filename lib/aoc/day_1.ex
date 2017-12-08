5bf6ba542b4b5ca23d8132422ea3f9621a69b86c
Norbert Melzer <timmelzer@gmail.com>
2017-12-08 22:23:06 +0100

    day1: clean dead code and commentary

--- lib/aoc/day_1.ex
+++ lib/aoc/day_1.ex
@@ -20,15 +20,7 @@ defmodule AoC15.Day1 do
   defp diff_count_parens("(" <> t, acc), do: diff_count_parens(t, acc + 1)
   defp diff_count_parens(")" <> t, acc), do: diff_count_parens(t, acc - 1)
 
-  defp count_parens(input, acc \\ {0, 0})
-  defp count_parens("", acc), do: acc
-  defp count_parens("(" <> input, {o, c}), do: count_parens(input, {o + 1, c})
-  defp count_parens(")" <> input, {o, c}), do: count_parens(input, {o, c + 1})
-
-  defp diff_parens({o, c}), do: o - c
-
   defp find_floor(input, target), do: find_floor(input, target, 0, 0)
-  #  defp find_floor(input, target, idx \\ 0, current \\ 0)
   defp find_floor(_input, target, idx, target), do: idx
   defp find_floor("(" <> input, target, idx, current), do: find_floor(input, target, idx + 1, current + 1)
   defp find_floor(")" <> input, target, idx, current), do: find_floor(input, target, idx + 1, current - 1)
